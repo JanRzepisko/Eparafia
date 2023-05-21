fe68f2a04798311ddfe213712dd184a6903ff715
Jan Rzepisko <60238932+JanRzepisko@users.noreply.github.com>
2023-05-21 11:57:01 +0200

    comment user controller, fix authorize in indentity (#33)

--- Eparafia.Identity.API/Controllers/PriestController.cs
+++ Eparafia.Identity.API/Controllers/PriestController.cs
@@ -1,25 +1,31 @@
 using Eparafia.Identity.Application.Actions.Priest;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Shared.BaseModels.ApiControllerModels;
+using Shared.BaseModels.Jwt;
 
 namespace Eparafia.Identity.API.Controllers;
 
+[Authorize(JwtPolicies.Priest)]
 [ApiController]
 [Route("Priest")]
 public class PriestController : BaseApiController
 {
     public PriestController(IMediator mediator) : base(mediator) { }
 
+    [AllowAnonymous]
     [HttpPost]
     [Route("Login")]
     public Task<IActionResult> Login(LoginPriest.Query request) => Endpoint(request);
+
+    [AllowAnonymous]
+    [HttpPost]
+    public Task<IActionResult> RegisterUser(RegisterPriest.Command request) => Endpoint(request);    
+    
     [HttpPost]
     [Route("RefreshToken")]
     public Task<IActionResult> RefreshToken(RefreshToken.Query request) => Endpoint(request);
-    
-    [HttpPost]
-    public Task<IActionResult> RegisterUser(RegisterPriest.Command request) => Endpoint(request);    
     
     [HttpPut]
     public Task<IActionResult> UpdateUser(UpdatePriest.Command request) => Endpoint(request);
--- Eparafia.Identity.API/Controllers/UserController.cs
+++ Eparafia.Identity.API/Controllers/UserController.cs
@@ -1,52 +1,52 @@
-using Eparafia.Identity.Application.Actions.Priest;
-using Eparafia.Identity.Application.Actions.User;
-using MediatR;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Shared.BaseModels.ApiControllerModels;
+//using Eparafia.Identity.Application.Actions.Priest;
+//using Eparafia.Identity.Application.Actions.User;
+//using MediatR;
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Mvc;
+//using Shared.BaseModels.ApiControllerModels;
 
-namespace Eparafia.Identity.API.Controllers;
+//namespace Eparafia.Identity.API.Controllers;
 
-[ApiController]
-[Route("User")]
-[Authorize]
-public class UserController : BaseApiController
-{
-    public UserController(IMediator mediator) : base(mediator)
-    {
-    }
+//[ApiController]
+//[Route("User")]
+//[Authorize]
+//public class UserController : BaseApiController
+//{
+//    public UserController(IMediator mediator) : base(mediator)
+//    {
+//    }
 
-    [HttpPost]
-    [AllowAnonymous]
-    [Route("Login")]
-    public Task<IActionResult> Login(LoginUser.Query request)
-    {
-        return Endpoint(request);
-    }
+//    [HttpPost]
+//    [AllowAnonymous]
+//    [Route("Login")]
+//    public Task<IActionResult> Login(LoginUser.Query request)
+//    {
+//        return Endpoint(request);
+//    }
 
-    [HttpPost]
-    [AllowAnonymous]
-    [Route("RefreshToken")]
-    public Task<IActionResult> RefreshToken(RefreshToken.Query request)
-    {
-        return Endpoint(request);
-    }
+//    [HttpPost]
+//    [AllowAnonymous]
+//    [Route("RefreshToken")]
+//    public Task<IActionResult> RefreshToken(RefreshToken.Query request)
+//    {
+//        return Endpoint(request);
+//    }
 
-    [HttpPost]
-    public Task<IActionResult> RegisterUser(RegisterUser.Command request)
-    {
-        return Endpoint(request);
-    }
+//    [HttpPost]
+//    public Task<IActionResult> RegisterUser(RegisterUser.Command request)
+//    {
+//        return Endpoint(request);
+//    }
 
-    [HttpPut]
-    public Task<IActionResult> UpdateUser(UpdateUser.Command request)
-    {
-        return Endpoint(request);
-    }
+//    [HttpPut]
+//    public Task<IActionResult> UpdateUser(UpdateUser.Command request)
+//    {
+//        return Endpoint(request);
+//    }
 
-    [HttpDelete]
-    public Task<IActionResult> DeleteUser(RemoveUser.Command request)
-    {
-        return Endpoint(request);
-    }
-}
+//    [HttpDelete]
+//    public Task<IActionResult> DeleteUser(RemoveUser.Command request)
+//    {
+//        return Endpoint(request);
+//    }
+//}
